1aaa545afcc7e789c832b5b1fa69c2af8c12d7fd
abidkhan263187 <abidkhan263187@gmail.com>
2023-11-02 13:58:51 +0530

    add

--- Routes/ProductRoute.js
+++ Routes/ProductRoute.js
@@ -7,17 +7,17 @@ productRoute.get('/', async (req, res) => {
     const { sortby, order, pageNo, limit,category } = req.query;
 
     const currPage = parseInt(pageNo || 1);
-    const currLimit = parseInt(limit || 3);
+    const currLimit = parseInt(limit || 6);
     const skip = (currPage - 1) * currLimit;
     try {
         let list;
-        if (sortby && order && category) {
-           const ordering = order == 'asc' ? 1 : -1;
+        if (sortby && order!=="" && category) {
+           const ordering = order === 'asc' ? 1 : -1;
             list = await Product.find({category:category}).sort({ [sortby]: ordering }).skip(skip).limit(currLimit)
-        }else if (sortby && order ) {
+        }else if (sortby && order!=="" ) {
             const ordering = order == 'asc' ? 1 : -1;
              list = await Product.find().sort({ [sortby]: ordering }).skip(skip).limit(currLimit)
-         }
+         } 
         else{
             list = await Product.find(req.query).skip(skip).limit(currLimit)
         }
